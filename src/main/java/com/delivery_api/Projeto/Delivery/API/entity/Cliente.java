bbadcc795f0b3d09b1894eab304abb28537f480e
lawrindovsk <gustavo.laurindo.santos@gmail.com>
2025-11-20 14:37:15 -0300

    Classes Cliente, Restaurante, Pedido, Produto Foram criados, banco de dados h2 funcionando.

--- src/main/java/com/delivery_api/Projeto/Delivery/API/entity/Cliente.java
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/entity/Cliente.java
@@ -13,49 +13,36 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     private String nome;
     private String email;
-    private boolean ativo;
+    private String endereco;
+    private String telefone;
+    private Boolean ativo = true;
 
-    public Cliente(){
-        //JPA PRECISA DE CONSTRUTOR (MESMO ELE VAZIO)
+    //Construtores (necessário para o JPA/DataLoader)
+    // 1. Construtor Vazio (OBRIGATÓRIO para o JPA/Hibernate funcionar)
+    public Cliente() {
     }
 
-    public Cliente(String nome, String email){
+    // 2. Construtor com argumentos (Para o seu DataLoader funcionar)
+    public Cliente(String nome, String email) {
         this.nome = nome;
         this.email = email;
-        this.ativo = true;
-    }
-    public Long getID(){
-        return id;
-    }
-    public void setID(Long id){
-        this.id = id;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
     }
 
 
-    public boolean getAtivo() {
-        return ativo;
-    }
 
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+    //Get e Set;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public Boolean getAtivo() { return ativo; }
+    public void setAtivo(Boolean ativo) { this.ativo = ativo; }
 }
