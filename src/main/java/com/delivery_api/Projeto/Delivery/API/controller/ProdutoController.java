862d118b6dce12e4747348a0b7502baf6697bfc9
lawrindovsk <gustavo.laurindo.santos@gmail.com>
2025-11-21 20:27:35 -0300

    Service e Controller da entidade Produto criados, sendo capaz de dar GET e POST em produtos especificos, igual um aplicativo de celular.

---
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/controller/ProdutoController.java
@@ -0,0 +1,44 @@
+package com.delivery_api.Projeto.Delivery.API.controller;
+
+import com.delivery_api.Projeto.Delivery.API.entity.Produto;
+import com.delivery_api.Projeto.Delivery.API.service.ProdutoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api") // Note que aqui usei só /api para poder variar as rotas abaixo
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    // 1. Cadastrar Produto (POST /api/produtos)
+    @PostMapping("/produtos")
+    public ResponseEntity<Produto> cadastrar(@RequestBody Produto produto) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(produtoService.salvar(produto));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build(); // Retorna 400 se o restaurante não existir
+        }
+    }
+
+    // 2. Listar Todos (GET /api/produtos)
+    @GetMapping("/produtos")
+    public List<Produto> listarTodos() {
+        return produtoService.listarTodos();
+    }
+
+    // 3. Buscar Produtos de um Restaurante Específico
+    // URL: /api/restaurantes/{idRestaurante}/produtos
+    @GetMapping("/restaurantes/{restauranteId}/produtos")
+    public List<Produto> buscarPorRestaurante(@PathVariable Long restauranteId) {
+        return produtoService.buscarPorRestaurante(restauranteId);
+    }
+}
